1fb0c115e1eb2264a2ccfed2ee4704b210fed51d
23981a44u1-design <23981a44u1@raghuenggcollege.in>
2026-03-18 21:17:32 +0530

    Update app.py

--- vedhapro/app.py
+++ vedhapro/app.py
@@ -1,19 +1,18 @@
-app.py
 from flask import Flask, render_template
 
 app = Flask(
     __name__,
-    template_folder="../frontend/templates",
-    static_folder="../frontend/static"
+    template_folder="frontend/templates",
+    static_folder="frontend/static"
 )
 
 @app.route("/")
 def home():
-    return render_template("w.html")   # first page
+    return render_template("w.html")
 
 @app.route("/wish")
 def wish_page():
-    return render_template("index.html")   # second page
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
